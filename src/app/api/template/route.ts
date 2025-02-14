6baca8f5031babfe2be7a8941f1bab3c6057019f
Rudendru-03 <rahul.squaregroup.tech>
2025-02-14 20:21:26 +0530

    temple route file changed

--- src/app/api/template/route.ts
+++ src/app/api/template/route.ts
@@ -46,7 +46,7 @@ export async function POST(req: NextRequest) {
     };
 
     const response = await fetch(
-      "https://graph.facebook.com/v19.0/${PHONE_NUMBER_ID}/messages",
+      `https://graph.facebook.com/v19.0/${PHONE_NUMBER_ID}/messages`,
       {
         method: "POST",
         headers: {
